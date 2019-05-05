683fa503eb263580ccd40885b515829c0291dc5e
Jerome Gaigne <jerome.gaigne@etu.utc.fr>
2019-05-05 20:40:29 +0200

    Update BeatData class

--- Rythm School/Assets/Scripts/BeatData.cs
+++ Rythm School/Assets/Scripts/BeatData.cs
@@ -4,5 +4,6 @@ public class BeatData
     public float Timer;
     public BeatInput[] Inputs;
     public int StateMachineNumber;
+    public int StateMachineType;
     public int StateMachineValue;
 }
