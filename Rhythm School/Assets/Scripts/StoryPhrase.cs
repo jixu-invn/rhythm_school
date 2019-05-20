4e36134044120e397cc2abe6a8c496f87017f909
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2019-05-21 00:12:37 +0200

    Upload New File

---
+++ Rhythm School/Assets/Scripts/StoryPhrase.cs
@@ -0,0 +1,9 @@
+﻿[System.Serializable]
+public class StoryPhrase
+{
+    public enum character {Cleo, Chord, Audra, Aladdin}
+    public enum emotion {Neutral, Stressed, Angry, Happy, Euphoric, Pretentious, Evasive, Sad, Desperate, Panicked, Laughing, Shouting};
+    public string Phrase;
+    public character CurrentCharacter;
+    public emotion[] EmotionTab;
+}
