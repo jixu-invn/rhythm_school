58e505d0acb7b5e9e3126c8bddaa24b123928ef3
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2019-05-20 21:21:29 +0200

    Upload New File

---
+++ Rhythm School/Assets/Scripts/StoryData.cs
@@ -0,0 +1,26 @@
+﻿[System.Serializable]
+public class StoryData
+{
+    public StoryPhrase[] Dialog;
+    private int current = 0;
+
+    public int GetIndex()
+    {
+        return current;
+    }
+
+    public StoryPhrase GetCurrent()
+    {
+        return Dialog[current];
+    }
+
+    public void Next()
+    {
+        current++;
+    }
+
+    public StoryPhrase End()
+    {
+        return Dialog[Dialog.Length - 1];
+    }
+}
