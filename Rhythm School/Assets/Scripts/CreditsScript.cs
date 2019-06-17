5129434ba06a5229b4340ba4a8600ce55ead7cca
Jerome Gaigne <jerome.gaigne@etu.utc.fr>
2019-06-18 00:26:28 +0200

    credits

---
+++ Rhythm School/Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(UnityEngine.Video.VideoPlayer))]
+public class CreditsScript : MonoBehaviour
+{
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+
+    bool isPlaying = false;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+    }
+
+    private void Update()
+    {
+        if ((isPlaying && videoPlayer.isPlaying == false))
+            Next();
+
+
+        isPlaying = videoPlayer.isPlaying;
+    }
+
+    private void Next()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+    }
+}
